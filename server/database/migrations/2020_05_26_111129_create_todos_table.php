c5a94a44f77b2b581ae8617dfdfd07ac7e406a61
hiroaki-kitagawa <hirock17l@icloud.com>
2020-05-26 20:25:29 +0900

    migration error

---
+++ server/database/migrations/2020_05_26_111129_create_todos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->inteer('user_id');
+            $table->string('title');
+            $table->string('detail');
+            $table->integer('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
