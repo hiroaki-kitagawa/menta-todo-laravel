f0bf64370c62c9e6835f5d2be4ec25d32b0f98b4
hiroaki-kitagawa <hirock17l@icloud.com>
2020-05-28 20:48:28 +0900

    add seeder

--- server/routes/web.php
+++ server/routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('todos/{id}', 'TodoController@show');
+// Route::post('todos', 'TodoController@store');
+// Route::put('todos/{id}', 'TodoController@update');
+// Route::delete('todos/{id}', 'TodoController@destroy');
+// Route::get('todos', 'TodoController@index')->name('todos.index');
+// Route::get('todos/create', 'TodoController@create')->name('todos.create');
+// Route::get('todos/{id}/edit', 'TodoController@edit')->name('todos.edit');
+
+Route::resource('todos', 'TodoController');
